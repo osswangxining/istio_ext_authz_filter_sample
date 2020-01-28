ee9769d4d0f72a9c93d518af50a1ca364cf28177
Xi Ning Wang <osswangxining@163.com>
2020-01-28 19:22:32 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v2"
+	envoy_type "github.com/envoyproxy/go-control-plane/envoy/type"
+	"github.com/gogo/googleapis/google/rpc"
+	"google.golang.org/grpc"
+)
+
+// empty struct because this isn't a fancy example
+type AuthorizationServer struct{}
+
+// inject a header that can be used for future rate limiting
+func (a *AuthorizationServer) Check(ctx context.Context, req *auth.CheckRequest) (*auth.CheckResponse, error) {
+	authHeader, ok := req.Attributes.Request.Http.Headers["authorization"]
+	var splitToken []string
+	if ok {
+		splitToken = strings.Split(authHeader, "Bearer ")
+	}
+	if len(splitToken) == 2 {
+		token := splitToken[1]
+		// Normally this is where you'd go check with the system that knows if it's a valid token.
+
+		if strings.HasPrefix(token, "asm-") {
+			return &auth.CheckResponse{
+				Status: &rpc.Status{
+					Code: int32(rpc.OK),
+				},
+				HttpResponse: &auth.CheckResponse_OkResponse{
+					OkResponse: &auth.OkHttpResponse{
+						Headers: []*core.HeaderValueOption{
+							{
+								Header: &core.HeaderValue{
+									Key:   "x-custom-header-from-authz",
+									Value: "some value",
+								},
+							},
+						},
+					},
+				},
+			}, nil
+		}
+	}
+	return &auth.CheckResponse{
+		Status: &rpc.Status{
+			Code: int32(rpc.UNAUTHENTICATED),
+		},
+		HttpResponse: &auth.CheckResponse_DeniedResponse{
+			DeniedResponse: &auth.DeniedHttpResponse{
+				Status: &envoy_type.HttpStatus{
+					Code: envoy_type.StatusCode_Unauthorized,
+				},
+				Body: "Need an Authorization Header with a character bearer token using asm- as prefix!",
+			},
+		},
+	}, nil
+}
+
+func main() {
+	// create a TCP listener on port 4000
+	lis, err := net.Listen("tcp", ":4000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("listening on %s", lis.Addr())
+
+	grpcServer := grpc.NewServer()
+	authServer := &AuthorizationServer{}
+	auth.RegisterAuthorizationServer(grpcServer, authServer)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
+}
